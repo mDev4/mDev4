7f9259bb34c11c1aa7d8dd9c4b6c7c1bbec5e48a
Lars Kuijlenburg <lars.kuijlenburg@gmail.com>
2016-03-30 15:16:58 +0200

    Login according to users in database now possible. messages need to be fixed

--- KVS/Shared/Database/Managers/UserControl.cs
+++ KVS/Shared/Database/Managers/UserControl.cs
@@ -20,7 +20,7 @@ namespace Shared.Database.Managers
                 {
                     using (SqlCommand command = new SqlCommand(
                         "INSERT INTO [User] VALUES(@username, @password, @firstname, @lastname, @email, @phoneNumber)", con))
-                    {                        
+                    {
                         command.Parameters.Add(new SqlParameter("username", user.Username));
                         command.Parameters.Add(new SqlParameter("password", user.Password));
                         command.Parameters.Add(new SqlParameter("firstname", user.Firstname));
@@ -30,12 +30,41 @@ namespace Shared.Database.Managers
                         command.ExecuteNonQuery();
                     }
                 }
-                catch(SqlException ex)
+                catch (SqlException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
         }
 
+        public static UserModel getUserByUsername(string username)
+        {
+            UserModel user = new UserModel();
+            using (SqlConnection con = new SqlConnection(DatabaseHelper.dbString))
+            {
+                con.Open();
+
+                using (SqlCommand command = new SqlCommand(
+                    "SELECT * FROM [User] WHERE username = @username", con))
+                {
+                    command.Parameters.Add(new SqlParameter("username", username));
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        user.Id = reader.GetInt32(0);
+                        user.Username = reader.GetString(1);
+                        user.Password = reader.GetString(2);
+                        user.Firstname = reader.GetString(3);
+                        user.Lastname = reader.GetString(4);
+                        user.Email = reader.GetString(5);
+                        user.PhoneNumber = reader.GetString(6);
+
+                        return user;
+                    }
+                }
+            }
+            return user;
+        }
     }
 }
