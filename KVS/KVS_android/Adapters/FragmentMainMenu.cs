33d19ff9d9e65598acf0794fdc0b979a0571fd4a
Ryan Zammit <ryan.zammit@hva.nl>
2016-04-19 14:15:45 +0200

    Voorlopige menu , R klasse wordt niet geupdate bij mij , als iemand daar even bij kan helpen.

---
+++ KVS/KVS_android/Adapters/FragmentMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.Widget;
+
+namespace KVS_android
+{
+	public class FragmentMainMenu : Fragment
+	{
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			return inflater.Inflate(Resource.Layout.fragment_layout, container, false);
+		}
+
+
+	}
+}
+
