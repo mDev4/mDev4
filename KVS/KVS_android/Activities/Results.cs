b07a810f16795fe7ead2c6da8845e23a37007bcb
Ryan Zammit <ryan.zammit@hva.nl>
2016-05-10 14:58:29 +0200

    All the changes to the screens , can someone double check please?

---
+++ KVS/KVS_android/Activities/Results.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+
+namespace KVS_android.Activities
+{
+    [Activity(Label = "Resultaten")]
+    public class Results : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            base.SetTheme(Resource.Style.Base_V7_Theme_AppCompat);
+            SetContentView(Resource.Layout.insertResult);
+
+            var newFragment = new FragmentMainMenu();
+            var ft = FragmentManager.BeginTransaction();
+			ft.Add(Resource.Id.frameLayoutdetailedResult, newFragment);
+            ft.Commit();
+
+        }
+    }
+}
