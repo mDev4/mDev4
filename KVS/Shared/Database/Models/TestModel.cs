54fc0a53d71d61820c3475c0293a30c00bfb3d54
Lars Kuijlenburg <lars.kuijlenburg@gmail.com>
2016-05-17 13:57:24 +0200

    fixed?

---
+++ KVS/Shared/Database/Models/TestModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Database.Models
+{
+    class TestModel
+    {
+        private int id;
+        private string name;
+        private DateTime date;
+        private string subject;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+
+        public string Subject
+        {
+            get
+            {
+                return subject;
+            }
+            set
+            {
+                subject = value;
+            }
+        }
+    }
+}
---
+++ KVS/Shared/Database/Models/TestStudentModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Database.Models
+{
+    class TestStudentModel
+    {
+        private int student_id;
+        private int test_id;
+        private string grade;
+
+        public int Student_id
+        {
+            get
+            {
+                return student_id;
+            }
+            set
+            {
+                student_id = value;
+            }
+        }
+
+        public int Test_id
+        {
+            get
+            {
+                return test_id;
+            }
+            set
+            {
+                test_id = value;
+            }
+        }
+
+        public string Grade
+        {
+            get
+            {
+                return grade;
+            }
+            set
+            {
+                grade = value;
+            }
+        }
+    }
+}
