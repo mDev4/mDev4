f83a861466bb7edd4734b47333b164f3360fadde
Lars Kuijlenburg <lars.kuijlenburg@gmail.com>
2016-05-31 13:31:21 +0200

    tests got fixed

--- KVS/KVS_android/Activities/Tests.cs
+++ KVS/KVS_android/Activities/Tests.cs
@@ -6,13 +6,12 @@ using KVS_android;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Support.V4.App;
-using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Shared.Database.Models;
 using Shared.Database.Managers;
+using KVS_android.Adapters_and_Fragments;
 
 namespace KVS_android
 {
@@ -22,20 +21,17 @@ namespace KVS_android
         private List<TestModel> tests;
         private ListView testsList;
         private TestAdapter listAdapter;
+        private Button editDateButton;
+        private Button addTestsButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            base.SetTheme(Resource.Style.Base_V7_Theme_AppCompat);
 
-            // Set our view from the "Tests" layout resource
+            // Set our view from the "group" layout resource
             SetContentView(Resource.Layout.Tests);
 
-            // frameLayout setup
-            var newFragment = new FragmentMainMenu();
-            var ft = FragmentManager.BeginTransaction();
-            ft.Add(Resource.Id.frameLayout1, newFragment);
-            ft.Commit();
+            addTestsButton = FindViewById<Button>(Resource.Id.addTestsButton);
 
             tests = TestControl.getAllTests();
             testsList = FindViewById<ListView>(Android.Resource.Id.List);
@@ -44,7 +40,35 @@ namespace KVS_android
 
             listAdapter.NotifyDataSetChanged();
 
+            //button logic
+            editDateButton = FindViewById<Button>(Resource.Id.selectDateButton);
+            editDateButton.Click += dateSelectOnClick;
+            addTestsButton.Click += addTestButtonOnClick;
 
         }
+
+        void dateSelectOnClick(object sender, EventArgs eventArgs) {
+            
+            DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime time)
+            {
+                editDateButton.Text = time.Date.ToString("MM-dd-yyyy");
+                tests = TestControl.getTestsByDate(editDateButton.Text);
+
+                listAdapter = new TestAdapter(this, tests);
+                testsList.Adapter = listAdapter;
+                listAdapter.NotifyDataSetChanged();
+            });
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
+            }
+
+       public void addTestButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(this, typeof(addTest));
+            StartActivity(intent);
+            
+        }
+
+
+        
     }
 }
